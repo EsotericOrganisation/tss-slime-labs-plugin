2e2780c1178f781fa4eaa92c2a66d0154df02974
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-02 19:56:42 +0100

    📄 Formatting and style changes.

--- src/main/java/net/slqmy/tss_slimelabs/SlimeLabsPlugin.java
+++ src/main/java/net/slqmy/tss_slimelabs/SlimeLabsPlugin.java
@@ -4,13 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SlimeLabsPlugin extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
-    }
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+	}
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
 }
