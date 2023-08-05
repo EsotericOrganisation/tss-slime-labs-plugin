d70754fe1a58640085971ce51cdcea96c8c80514
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-05 01:34:17 +0100

    📋 Improved launch command testing.

--- src/main/java/net/slqmy/tss_slimelabs/command/LaunchCommand.java
+++ src/main/java/net/slqmy/tss_slimelabs/command/LaunchCommand.java
@@ -1,131 +1,207 @@
 package net.slqmy.tss_slimelabs.command;
 
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.FloatArgument;
+import dev.jorel.commandapi.arguments.BooleanArgument;
+import dev.jorel.commandapi.arguments.DoubleArgument;
+import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.executors.CommandArguments;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.util.TriState;
-import net.minecraft.core.BlockPos;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.minecraft.ChatFormatting;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 import net.slqmy.tss_core.type.Colour;
-import net.slqmy.tss_core.util.DebugUtil;
 import net.slqmy.tss_core.util.MessageUtil;
 import net.slqmy.tss_core.util.NMSUtil;
-import net.slqmy.tss_core.util.ReflectUtil;
 import net.slqmy.tss_slimelabs.SlimeLabsPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.scoreboard.*;
 import org.bukkit.util.Vector;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class LaunchCommand {
 
+	private final ArrayList<BukkitTask> spawnParticlesTasks = new ArrayList<>();
+	private BukkitTask analysisTask;
+
 	public LaunchCommand(SlimeLabsPlugin plugin) {
+		ScoreboardManager boardManager = Bukkit.getScoreboardManager();
+
 		new CommandAPICommand("launch")
-						.withArguments(new FloatArgument("force"))
+						.withOptionalArguments(new EntitySelectorArgument.OneEntity("entity"), new DoubleArgument("velocity"), new BooleanArgument("finish on landing"))
 						.executesPlayer((Player player, CommandArguments args) -> {
-							player.sendMessage(
-											Component.text("--- ", Colour.SKY_BLUE.asTextColour())
-															.append(Component.text("Launch", Colour.SLIME.asTextColour()))
-															.append(Component.text(" ---", Colour.SKY_BLUE.asTextColour()))
-							);
+							if (analysisTask != null) {
+								player.sendMessage(Component.text("There is already a launch analysis task active!", Colour.RED.asTextColour()));
+								return;
+							}
+
+							TextComponent launchInfoTitle = Component.text("----- ", Colour.SKY_BLUE.asTextColour())
+											.append(Component.text("Launch", Colour.SLIME.asTextColour(), TextDecoration.UNDERLINED))
+											.append(Component.text(" -----", Colour.SKY_BLUE.asTextColour()));
+
+							player.sendMessage(launchInfoTitle);
+
+							Boolean finishOnLanding = (Boolean) args.get("finish on landing");
+
+							if (finishOnLanding == null || !finishOnLanding) {
+								TextComponent finishLaunchAnalysis = Component.text("[Click here to finish launch and view statistics]", Colour.SLIME.asTextColour());
+								finishLaunchAnalysis = finishLaunchAnalysis.clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/finish-launch"));
+								finishLaunchAnalysis = finishLaunchAnalysis.hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Click to stop launch analysis!", Colour.YELLOW.asTextColour())));
+
+								player.sendMessage(finishLaunchAnalysis);
+							}
+
+							Scoreboard board = boardManager.getNewScoreboard();
+							Objective objective = board.registerNewObjective("launch_info", Criteria.DUMMY, launchInfoTitle);
+							objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+							TextComponent pipe = MessageUtil.getPipeTextComponent();
+							TextComponent colon = MessageUtil.getColonTextComponent();
+
+							Team time = board.registerNewTeam("Time");
+							time.addEntry(ChatFormatting.YELLOW.toString());
+							time.prefix(Component.text("Time", Colour.LIGHT_GREY.asTextColour()).append(colon));
+
+							Team xxa = board.registerNewTeam("xxa");
+							xxa.addEntry(ChatFormatting.RED.toString());
+							xxa.prefix(pipe.append(Component.text("xxa", Colour.RED.asTextColour())).append(colon));
+							xxa.suffix(Component.text("0", Colour.ORANGE.asTextColour()));
+
+							Team yya = board.registerNewTeam("yya");
+							yya.addEntry(ChatFormatting.GREEN.toString());
+							yya.prefix(pipe.append(Component.text("yya", Colour.SLIME.asTextColour())).append(colon));
+							yya.suffix(Component.text("0", Colour.ORANGE.asTextColour()));
+
+							Team zza = board.registerNewTeam("zza");
+							zza.addEntry(ChatFormatting.AQUA.toString());
+							zza.prefix(pipe.append(Component.text("zza", Colour.SKY_BLUE.asTextColour())).append(colon));
+							zza.suffix(Component.text("0", Colour.ORANGE.asTextColour()));
+
+							Team vx = board.registerNewTeam("vx");
+							vx.addEntry(ChatFormatting.RED.toString() + ChatFormatting.RED);
+							vx.prefix(Component.text("vx", Colour.RED.asTextColour()).append(colon));
+
+							Team vz = board.registerNewTeam("vz");
+							vz.addEntry(ChatFormatting.AQUA.toString() + ChatFormatting.AQUA);
+							vz.prefix(Component.text("vz", Colour.SKY_BLUE.asTextColour()).append(colon));
+
+							Team vy = board.registerNewTeam("vy");
+							vy.addEntry(ChatFormatting.GREEN.toString() + ChatFormatting.GREEN);
+							vy.prefix(Component.text("vy", Colour.SLIME.asTextColour()).append(colon));
+
+							objective.getScore(ChatFormatting.YELLOW.toString()).setScore(10);
+							objective.getScore("").setScore(9);
+							objective.getScore(ChatFormatting.RED.toString()).setScore(8);
+							objective.getScore(ChatFormatting.GREEN.toString()).setScore(7);
+							objective.getScore(ChatFormatting.AQUA.toString()).setScore(6);
+							objective.getScore(" ").setScore(5);
+							objective.getScore(ChatFormatting.RED.toString() + ChatFormatting.RED).setScore(4);
+							objective.getScore(ChatFormatting.AQUA.toString() + ChatFormatting.AQUA).setScore(3);
+							objective.getScore("  ").setScore(2);
+							objective.getScore(ChatFormatting.GREEN.toString() + ChatFormatting.GREEN).setScore(1);
+
+							Entity entity = (Entity) args.get("entity");
+							if (entity == null) {
+								entity = player;
+							}
+
+							Double velocity = (Double) args.get("velocity");
+							if (velocity == null) {
+								velocity = 1D;
+							}
+
+							entity.setVelocity(entity.getLocation().getDirection().multiply(velocity));
+
+							player.setScoreboard(board);
 
 							ServerPlayer serverPlayer = NMSUtil.getServerPlayer(player);
 							assert serverPlayer != null;
 
-							boolean effectiveAI = serverPlayer.isEffectiveAi();
-							boolean controlledByLocalInstance = serverPlayer.isControlledByLocalInstance();
-							boolean shouldDiscardFriction = serverPlayer.shouldDiscardFriction();
+							final int[] ticks = {0};
 
-							DebugUtil.log(ReflectUtil.getFieldValue(serverPlayer, "bU"));
+							Entity finalEntity = entity;
+							net.minecraft.world.entity.Entity nmsEntity = (net.minecraft.world.entity.Entity) NMSUtil.invokeHandle(entity);
+							assert nmsEntity != null;
 
-							boolean discardFriction = (boolean) ReflectUtil.getFieldValue(serverPlayer, "bU", Boolean.TYPE);
-							TriState frictionState = serverPlayer.frictionState;
-
-							float force = (float) args.get(0);
-
-							Vector playerDirection = player.getLocation().getDirection().multiply(force);
-							Vec3 initialVelocity = new Vec3(
-											playerDirection.getX(),
-											playerDirection.getY(),
-											playerDirection.getZ()
-							);
-
-							Level level = serverPlayer.level();
-							boolean levelIsClientSide = level.isClientSide();
-
-							float xxa = serverPlayer.xxa;
-							float yya = serverPlayer.yya;
-							float zza = serverPlayer.zza;
-
-							Method getBlockPosBelowThatAffectsMyMovement = ReflectUtil.getAccessibleMethod(serverPlayer, "aE");
-							assert getBlockPosBelowThatAffectsMyMovement != null;
-
-							BlockPos pos;
-
-							try {
-								pos = (BlockPos) getBlockPosBelowThatAffectsMyMovement.invoke(serverPlayer);
-							} catch (IllegalAccessException | InvocationTargetException exception) {
-								DebugUtil.handleException("Couldn't invoke method 'getBlockPosBelowThatAffectsMyMovement' of serverPlayer object " + serverPlayer + "!", exception);
-								return;
-							}
-
-							float friction = level.getBlockState(pos).getBlock().getFriction();
-
-							Vec3 handledFriction = serverPlayer.handleRelativeFrictionAndCalculateMovement(initialVelocity, friction);
-
-							Object[] values = new Object[]{effectiveAI, controlledByLocalInstance, pos, friction, shouldDiscardFriction, discardFriction, frictionState, handledFriction, levelIsClientSide, xxa, yya, zza};
-							String[] valueNames = new String[]{"Effective AI", "Controlled by local instance", "Block position below that affects my movement", "Friction", "Should discard friction", "Discard friction", "Friction state", "Handled friction", "Level is client side", "xxa", "yya", "zza"};
-
-							for (int i = 0; i < values.length; i++) {
-								player.sendMessage(
-												Component.text("- " + valueNames[i] + ": ", Colour.ORANGE.asTextColour())
-																.append(MessageUtil.format(values[i]))
-								);
-							}
-
-							serverPlayer.setDeltaMovement(initialVelocity);
-							serverPlayer.setOnGroundWithKnownMovement(false, initialVelocity);
-
-							new BukkitRunnable() {
+							analysisTask = new BukkitRunnable() {
 
 								@Override
 								public void run() {
-									DebugUtil.log("Player is on ground? ", serverPlayer.onGround());
+									ticks[0] += 1;
 
-									if (serverPlayer.onGround()) {
-										cancel();
-										return;
-									}
+									xxa.suffix(Component.text(MessageUtil.formatNumber(serverPlayer.xxa), Colour.ORANGE.asTextColour()));
+									yya.suffix(Component.text(MessageUtil.formatNumber(serverPlayer.yya), Colour.ORANGE.asTextColour()));
+									zza.suffix(Component.text(MessageUtil.formatNumber(serverPlayer.zza), Colour.ORANGE.asTextColour()));
 
-									new BukkitRunnable() {
+									Vector velocity = finalEntity.getVelocity();
 
-										private final Location particleLocation = player.getLocation();
+									vx.suffix(Component.text(MessageUtil.formatNumber(velocity.getX()), Colour.LIGHT_GREY.asTextColour()));
+									vz.suffix(Component.text(MessageUtil.formatNumber(velocity.getZ()), Colour.LIGHT_GREY.asTextColour()));
+
+									vy.suffix(Component.text(MessageUtil.formatNumber(velocity.getY()), Colour.LIGHT_GREY.asTextColour()));
+
+									time.suffix(
+													Component.text(
+																					MessageUtil.formatNumber(ticks[0] / 20D) + "s", Colour.YELLOW.asTextColour()
+																	)
+																	.append(Component.text(" (", Colour.LIGHT_GREY.asTextColour()))
+																	.append(Component.text(ticks[0] + "t", Colour.SKY_BLUE.asTextColour()))
+																	.append(Component.text(")", Colour.LIGHT_GREY.asTextColour()))
+									);
+
+									spawnParticlesTasks.add(new BukkitRunnable() {
+
+										private final Location particleLocation = finalEntity.getLocation();
 
 										@Override
 										public void run() {
-											if (serverPlayer.onGround()) {
-												cancel();
-												return;
-											}
-
 											particleLocation.getWorld().spawnParticle(Particle.DUST_COLOR_TRANSITION, particleLocation, 1, new Particle.DustTransition(
 																			Color.RED,
 																			Color.RED,
 																			0.85F
 															)
 											);
+
+											if (finishOnLanding != null && finishOnLanding && serverPlayer.onGround()) {
+												cancel();
+											}
 										}
-									}.runTaskTimer(plugin, 0, 1);
+									}.runTaskTimer(plugin, 0, 1));
+
+									if (finishOnLanding != null && finishOnLanding && serverPlayer.onGround()) {
+										player.setScoreboard(boardManager.getNewScoreboard());
+										analysisTask = null;
+										cancel();
+									}
 								}
-							}.runTaskTimer(plugin, 1, 1);
+							}.runTaskTimer(plugin, 0, 1);
+						})
+						.register();
+
+		new CommandAPICommand("finish-launch")
+						.executesPlayer((Player player, CommandArguments args) -> {
+							if (analysisTask == null) {
+								player.sendMessage(Component.text("There is no analysis task active!", Colour.RED.asTextColour()));
+								return;
+							}
+
+							analysisTask.cancel();
+							analysisTask = null;
+							player.setScoreboard(boardManager.getNewScoreboard());
+
+							for (BukkitTask spawnParticlesTask : spawnParticlesTasks) {
+								spawnParticlesTask.cancel();
+							}
 						})
 						.register();
 	}
