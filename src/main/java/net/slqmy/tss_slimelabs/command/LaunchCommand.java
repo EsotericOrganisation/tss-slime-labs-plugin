f69834f4f977770dc62ac4a30b63b5f4169da70f
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-06 21:21:46 +0100

    📋 Added velocity estimation and error comparison.

--- src/main/java/net/slqmy/tss_slimelabs/command/LaunchCommand.java
+++ src/main/java/net/slqmy/tss_slimelabs/command/LaunchCommand.java
@@ -1,6 +1,7 @@
 package net.slqmy.tss_slimelabs.command;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
 import dev.jorel.commandapi.arguments.BooleanArgument;
 import dev.jorel.commandapi.arguments.DoubleArgument;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
@@ -9,13 +10,16 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
-import net.kyori.adventure.text.format.TextDecoration;
 import net.minecraft.ChatFormatting;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.phys.Vec3;
+import net.slqmy.tss_core.data.Message;
+import net.slqmy.tss_core.manager.MessageManager;
 import net.slqmy.tss_core.type.Colour;
+import net.slqmy.tss_core.util.DebugUtil;
 import net.slqmy.tss_core.util.MessageUtil;
 import net.slqmy.tss_core.util.NMSUtil;
-import net.slqmy.tss_slimelabs.SlimeLabsPlugin;
+import net.slqmy.tss_slimelabs.TSSSlimeLabsPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
@@ -26,37 +30,79 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.scoreboard.*;
 import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LaunchCommand {
 
+	final int[] ticks = new int[]{0};
+	private final ScoreboardManager boardManager = Bukkit.getScoreboardManager();
+	private final MessageManager messageManager;
 	private final ArrayList<BukkitTask> spawnParticlesTasks = new ArrayList<>();
+	private final AtomicReference<ArrayList<Boolean>> onGroundStats = new AtomicReference<>(new ArrayList<>());
+	private final List<VelocityEstimation> yVelocityEstimations = Arrays.asList(
+					(double initialVelocity, double acceleration, double drag, double ticksPassed) -> (initialVelocity - acceleration) * Math.pow(1 - drag, ticksPassed) - acceleration * (1 - Math.pow(1 - drag, ticksPassed)) / drag,
+					(double initialVelocity, double acceleration, double drag, double ticksPassed) -> initialVelocity * Math.pow(1 - drag, ticksPassed) - acceleration * (1 - Math.pow(1 - drag, ticksPassed)) / drag * (1 - drag),
+					(double initialVelocity, double acceleration, double drag, double ticksPassed) -> initialVelocity * Math.pow(1 - drag, ticksPassed + 1) - acceleration * (1 - Math.pow(1 - drag, ticksPassed + 1)) / drag * (1 - drag),
+					(double initialVelocity, double acceleration, double drag, double ticksPassed) -> Math.pow(50D / 49D, 1 - ticksPassed) + 98D / 25D * (Math.pow(49D / 50D, ticksPassed) - 1)
+	);
+	private final ArrayList<ArrayList<Double>> yVelocityEstimationErrors = new ArrayList<>();
+	private final List<VelocityEstimation> xzVelocityEstimations = List.of(
+					(double initialVelocity, double acceleration, double drag, double ticksPassed) -> initialVelocity * Math.pow(0.98D, ticksPassed)
+	);
+	private final ArrayList<ArrayList<Double>> xVelocityEstimationErrors = new ArrayList<>();
+	private final ArrayList<ArrayList<Double>> zVelocityEstimationErrors = new ArrayList<>();
 	private BukkitTask analysisTask;
 
-	public LaunchCommand(SlimeLabsPlugin plugin) {
-		ScoreboardManager boardManager = Bukkit.getScoreboardManager();
+	public LaunchCommand(@NotNull TSSSlimeLabsPlugin plugin) {
+		messageManager = plugin.getCore().getMessageManager();
 
 		new CommandAPICommand("launch")
+						.withShortDescription("Fling an entity in the sky!")
+						.withFullDescription("Flings an entity in the sky with a specified velocity. Analyses the flight path and returns some information about it once the analysis is done. Analysis finishes either on landing, when clicking the popup, or when the /finish-launch command is run. (see argument 3)")
+						.withUsage("/launch (entity) (velocity) (finish on landing)")
+						.withAliases("fling")
+						.withPermission(CommandPermission.OP)
 						.withOptionalArguments(new EntitySelectorArgument.OneEntity("entity"), new DoubleArgument("velocity"), new BooleanArgument("finish on landing"))
 						.executesPlayer((Player player, CommandArguments args) -> {
 							if (analysisTask != null) {
-								player.sendMessage(Component.text("There is already a launch analysis task active!", Colour.RED.asTextColour()));
+								player.sendMessage(messageManager.getPlayerMessage(Message.ACTIVE_LAUNCH_ANALYSIS_TASK, player));
 								return;
 							}
 
-							TextComponent launchInfoTitle = Component.text("----- ", Colour.SKY_BLUE.asTextColour())
-											.append(Component.text("Launch", Colour.SLIME.asTextColour(), TextDecoration.UNDERLINED))
-											.append(Component.text(" -----", Colour.SKY_BLUE.asTextColour()));
+							TextComponent launchInfoTitle = MessageUtil.createHeading(messageManager.getPlayerMessage(Message.LAUNCH, player));
 
 							player.sendMessage(launchInfoTitle);
+
+							Entity entity = (Entity) args.get("entity");
+							if (entity == null) {
+								entity = player;
+							}
+
+							TextComponent colon = MessageUtil.getColon();
+
+							player.sendMessage(messageManager.getPlayerMessage(Message.ENTITY, player)
+											.append(colon)
+											.append(Component.text(entity.getName(), Colour.LIGHT_GREY))
+							);
 
 							Boolean finishOnLanding = (Boolean) args.get("finish on landing");
 
 							if (finishOnLanding == null || !finishOnLanding) {
-								TextComponent finishLaunchAnalysis = Component.text("[Click here to finish launch and view statistics]", Colour.SLIME.asTextColour());
+								TextComponent leftBracket = MessageUtil.getLeftSquareBracket();
+								TextComponent rightBracket = MessageUtil.getRightSquareBracket();
+
+								TextComponent finishLaunchAnalysis = leftBracket.append(
+												messageManager.getPlayerMessage(Message.VIEW_LAUNCH_STATISTICS, player)
+																.append(rightBracket)
+								);
+
 								finishLaunchAnalysis = finishLaunchAnalysis.clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/finish-launch"));
-								finishLaunchAnalysis = finishLaunchAnalysis.hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Click to stop launch analysis!", Colour.YELLOW.asTextColour())));
+								finishLaunchAnalysis = finishLaunchAnalysis.hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, messageManager.getPlayerMessage(Message.STOP_LAUNCH_ANALYSIS, player)));
 
 								player.sendMessage(finishLaunchAnalysis);
 							}
@@ -65,12 +111,15 @@ public class LaunchCommand {
 							Objective objective = board.registerNewObjective("launch_info", Criteria.DUMMY, launchInfoTitle);
 							objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-							TextComponent pipe = MessageUtil.getPipeTextComponent();
-							TextComponent colon = MessageUtil.getColonTextComponent();
+							TextComponent pipe = MessageUtil.getPipe();
 
 							Team time = board.registerNewTeam("Time");
 							time.addEntry(ChatFormatting.YELLOW.toString());
-							time.prefix(Component.text("Time", Colour.LIGHT_GREY.asTextColour()).append(colon));
+							time.prefix(messageManager.getPlayerMessage(Message.TIME, player).append(colon));
+
+							Team isOnGround = board.registerNewTeam("On ground");
+							isOnGround.addEntry(ChatFormatting.GRAY.toString());
+							isOnGround.prefix(messageManager.getPlayerMessage(Message.ON_GROUND, player).append(colon));
 
 							Team vx = board.registerNewTeam("vx");
 							vx.addEntry(ChatFormatting.RED.toString() + ChatFormatting.RED);
@@ -84,77 +133,146 @@ public class LaunchCommand {
 							vy.addEntry(ChatFormatting.GREEN.toString() + ChatFormatting.GREEN);
 							vy.prefix(pipe.append(Component.text("vy", Colour.SLIME)).append(colon));
 
-							objective.getScore(ChatFormatting.YELLOW.toString()).setScore(6);
+							objective.getScore(ChatFormatting.YELLOW.toString()).setScore(7);
+							objective.getScore(ChatFormatting.GRAY.toString()).setScore(6);
 							objective.getScore("").setScore(5);
 							objective.getScore(ChatFormatting.RED.toString() + ChatFormatting.RED).setScore(4);
 							objective.getScore(ChatFormatting.AQUA.toString() + ChatFormatting.AQUA).setScore(3);
 							objective.getScore(" ").setScore(2);
 							objective.getScore(ChatFormatting.GREEN.toString() + ChatFormatting.GREEN).setScore(1);
 
-							Entity entity = (Entity) args.get("entity");
-							if (entity == null) {
-								entity = player;
-							}
-
 							Double velocity = (Double) args.get("velocity");
 							if (velocity == null) {
 								velocity = 1D;
 							}
 
-							entity.setVelocity(entity.getLocation().getDirection().multiply(velocity));
+							Vector initialVelocity = entity.getLocation().getDirection().multiply(velocity);
+							entity.setVelocity(initialVelocity);
+
+							double initialXVelocity = initialVelocity.getX();
+							double initialYVelocity = initialVelocity.getY();
+							double initialZVelocity = initialVelocity.getZ();
 
 							player.setScoreboard(board);
 
 							ServerPlayer serverPlayer = NMSUtil.getServerPlayer(player);
 							assert serverPlayer != null;
 
-							final int[] ticks = {0};
+							ticks[0] = 0;
 
 							Entity finalEntity = entity;
-							net.minecraft.world.entity.Entity nmsEntity = (net.minecraft.world.entity.Entity) NMSUtil.invokeHandle(entity);
+							net.minecraft.world.entity.Entity nmsEntity = (net.minecraft.world.entity.Entity) NMSUtil.invokeGetHandle(entity);
 							assert nmsEntity != null;
+
+							onGroundStats.set(new ArrayList<>());
+
+							for (int i = 0; i < yVelocityEstimations.size(); i++) {
+								yVelocityEstimationErrors.add(new ArrayList<>());
+							}
 
 							analysisTask = new BukkitRunnable() {
 
 								private Location previousLocation = finalEntity.getLocation();
 
+								private Vector previousVelocity = new Vector();
+								private Vec3 previousNmsVelocity = new Vec3(0, 0, 0);
+								private Vector previousRealVelocity = new Vector();
+
 								@Override
 								public void run() {
 									ticks[0] += 1;
 
+									if (finishOnLanding != null && finishOnLanding && ((finalEntity.isOnGround() && nmsEntity.onGround()) || (finalEntity.isDead() && !nmsEntity.isAlive()))) {
+										endLaunchAnalysis(player);
+										return;
+									}
+
 									Vector velocity = finalEntity.getVelocity();
 									Location currentLocation = finalEntity.getLocation();
 
-									TextComponent comma = Component.text(", ", Colour.GREY.asTextColour());
+									Vec3 nmsVelocity = nmsEntity.getDeltaMovement();
+
+									double realXVelocity = currentLocation.getX() - previousLocation.getX();
+									double realYVelocity = currentLocation.getY() - previousLocation.getY();
+									double realZVelocity = currentLocation.getZ() - previousLocation.getZ();
+
+									Vector realVelocity = new Vector(
+													realXVelocity,
+													realYVelocity,
+													realZVelocity
+									);
+
+									previousLocation = currentLocation;
+
+									for (int i = 0; i < yVelocityEstimations.size(); i++) {
+										VelocityEstimation estimation = yVelocityEstimations.get(i);
+										double estimatedVelocity = estimation.estimateVelocity(initialYVelocity, 0.08D, 0.02D, ticks[0]);
+										double error = estimatedVelocity - realYVelocity;
+
+										yVelocityEstimationErrors.get(i).add(error);
+
+										DebugUtil.log("Estimated y velocity: " + estimatedVelocity);
+									}
+
+									for (VelocityEstimation estimation : xzVelocityEstimations) {
+										DebugUtil.log("Estimated x velocity: " + estimation.estimateVelocity(initialXVelocity, 0.08D, 0.02D, ticks[0]));
+									}
+
+									for (VelocityEstimation estimation : xzVelocityEstimations) {
+										DebugUtil.log("Estimated z velocity: " + estimation.estimateVelocity(initialZVelocity, 0.08D, 0.02D, ticks[0]));
+									}
+
+									DebugUtil.log("Spigot velocity: " + velocity, "NMS velocity: " + nmsVelocity, "Real velocity: " + realVelocity);
+
+									time.suffix(
+													Component.text(
+																					MessageUtil.formatNumber(ticks[0] / 20D) + "s", Colour.YELLOW
+																	)
+																	.append(Component.text(" (", Colour.LIGHT_GREY))
+																	.append(Component.text(ticks[0] + "t", Colour.SKY_BLUE))
+																	.append(Component.text(")", Colour.LIGHT_GREY))
+									);
+
+									isOnGround.suffix(
+													MessageUtil.format(finalEntity.isOnGround())
+																	.append(Component.space())
+																	.append(pipe)
+																	.append(MessageUtil.format(nmsEntity.onGround))
+									);
+
+									onGroundStats.get().add(nmsEntity.onGround);
+
+
+									TextComponent comma = MessageUtil.getComma();
 
 									vx.suffix(
 													Component.text(MessageUtil.formatNumber(velocity.getX()), Colour.LIGHT_GREY)
 																	.append(comma)
-																	.append(Component.text(MessageUtil.formatNumber(currentLocation.getX() - previousLocation.getX())))
+																	.append(Component.text(MessageUtil.formatNumber(realXVelocity)))
+																	.append(comma)
+																	.append(Component.text(MessageUtil.formatNumber(nmsVelocity.x)))
 									);
 
 									vz.suffix(
 													Component.text(MessageUtil.formatNumber(velocity.getZ()), Colour.LIGHT_GREY)
 																	.append(comma)
-																	.append(Component.text(MessageUtil.formatNumber(currentLocation.getZ() - previousLocation.getZ())))
+																	.append(Component.text(MessageUtil.formatNumber(realZVelocity)))
+																	.append(comma)
+																	.append(Component.text(MessageUtil.formatNumber(nmsVelocity.z)))
 									);
 
 									vy.suffix(
 													Component.text(MessageUtil.formatNumber(velocity.getY()), Colour.LIGHT_GREY)
 																	.append(comma)
-																	.append(Component.text(MessageUtil.formatNumber(currentLocation.getY() - previousLocation.getY())))
+																	.append(Component.text(MessageUtil.formatNumber(realYVelocity)))
+																	.append(comma)
+																	.append(Component.text(MessageUtil.formatNumber(nmsVelocity.y)))
 									);
 
-									previousLocation = currentLocation;
+									previousVelocity = velocity;
+									previousNmsVelocity = nmsVelocity;
+									previousRealVelocity = realVelocity;
 
-									time.suffix(
-													Component.text(
-																					MessageUtil.formatNumber(ticks[0] / 20D) + "s", Colour.YELLOW.asTextColour()
-																	)
-																	.append(Component.text(" (", Colour.LIGHT_GREY.asTextColour()))
-																	.append(Component.text(ticks[0] + "t", Colour.SKY_BLUE.asTextColour()))
-																	.append(Component.text(")", Colour.LIGHT_GREY.asTextColour()))
-									);
 
 									spawnParticlesTasks.add(new BukkitRunnable() {
 
@@ -169,37 +287,46 @@ public class LaunchCommand {
 															)
 											);
 
-											if (finishOnLanding != null && finishOnLanding && serverPlayer.onGround()) {
-												cancel();
+											if (finishOnLanding != null && finishOnLanding && ((finalEntity.isOnGround() && nmsEntity.onGround()) || (finalEntity.isDead() && !nmsEntity.isAlive()))) {
+												endLaunchAnalysis(player);
 											}
 										}
 									}.runTaskTimer(plugin, 0, 1));
-
-									if (finishOnLanding != null && finishOnLanding && serverPlayer.onGround()) {
-										player.setScoreboard(boardManager.getNewScoreboard());
-										analysisTask = null;
-										cancel();
-									}
 								}
-							}.runTaskTimer(plugin, 1, 1);
+							}.runTaskTimer(plugin, 2, 1);
 						})
 						.register();
 
 		new CommandAPICommand("finish-launch")
-						.executesPlayer((Player player, CommandArguments args) -> {
-							if (analysisTask == null) {
-								player.sendMessage(Component.text("There is no analysis task active!", Colour.RED.asTextColour()));
-								return;
-							}
-
-							analysisTask.cancel();
-							analysisTask = null;
-							player.setScoreboard(boardManager.getNewScoreboard());
-
-							for (BukkitTask spawnParticlesTask : spawnParticlesTasks) {
-								spawnParticlesTask.cancel();
-							}
-						})
+						.executesPlayer((Player player, CommandArguments args) -> endLaunchAnalysis(player))
 						.register();
+	}
+
+	private void endLaunchAnalysis(Player player) {
+		if (analysisTask == null) {
+			player.sendMessage(messageManager.getPlayerMessage(Message.NO_ANALYSIS_TASK, player));
+			return;
+		}
+
+		analysisTask.cancel();
+		analysisTask = null;
+		player.setScoreboard(boardManager.getNewScoreboard());
+
+		for (BukkitTask spawnParticlesTask : spawnParticlesTasks) {
+			spawnParticlesTask.cancel();
+		}
+
+		spawnParticlesTasks.clear();
+
+		ArrayList<Boolean> onGroundStatsList = onGroundStats.get();
+
+		player.sendMessage(MessageUtil.createHeading(messageManager.getPlayerMessage(Message.LAUNCH_STATISTICS, player)));
+		player.sendMessage(MessageUtil.createCompactArray(onGroundStatsList.toArray()));
+
+		DebugUtil.log(yVelocityEstimationErrors);
+	}
+
+	private interface VelocityEstimation {
+		double estimateVelocity(double initialVelocity, double acceleration, double drag, double ticksPassed);
 	}
 }
